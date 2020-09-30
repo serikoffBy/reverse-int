3fde6334244d4135040d464b29ca3c0916c0bc24
serikoffBy <60609970+serikoffBy@users.noreply.github.com>
2020-09-30 19:36:46 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function reverse (n) {
     if (n < 0) {
-        return -1 * (+String(n).slice(1).split('').reverse().join(''));
+        return +String(n).slice(1).split('').reverse().join('');
     }
     else return +String(n).split('').reverse().join('');
 }
